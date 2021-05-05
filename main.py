0ab1e9979855cd90a8d914bd272b1c1c9083dc7f
AWieczarek <adamwie12@gmail.com>
2021-05-05 11:34:33 +0200

    test

---
+++ main.py
@@ -0,0 +1,67 @@
+from git import Repo
+import os
+import os.path
+
+path = '.'
+commit = 'test'
+site_path = 'https://awieczarek.github.io/StudentsGames/'
+classes_path = '.\\classes'
+title = '# StudentsGames'
+description = 'Games made by my students in Construct 3.'
+
+class Games():
+    def git_push(self):
+        try:
+            repo = Repo(path)
+            repo.git.add('.')
+            repo.index.commit(commit)
+            origin = repo.remote(name='origin')
+            origin.push()
+        except:
+            print('Error')
+
+    def dir_list(self, path):
+        final_path = classes_path + '\\' + path
+        dir_list = next(os.walk(final_path))[1]
+        return dir_list
+
+    def make_index(self, path):
+        dir = self.dir_list('')
+        for x in dir:
+            if not self.if_exists(x):
+                f = open(classes_path+ '\\' + x + '\\'+ "index.htm", "a")
+                template = open('start_html.txt', 'r')
+                lines = template.readlines()
+                for y in lines:
+                    f.write(y+'\n')
+                template.close()
+                f.write(classes_path+ '\\' + x)
+                template = open('end_html.txt', 'r')
+                lines = template.readlines()
+                for y in lines:
+                    f.write(y+'\n')
+                template.close()
+                f.close()
+
+    def add_to_readme(self):
+        f = open('README.md', 'w')
+        dirs = self.dir_list('')
+        f.write(title + '\n')
+        f.write(description + '\n')
+        for dir in dirs:
+            f.write('- ' + dir + '\n')
+
+
+    def if_exists(self, path):
+        try:
+            f = open(classes_path+ '\\'+ path + '\\'+ "index.htm")
+            f.close()
+            return True
+        except IOError:
+            return False
+
+
+if __name__ == "__main__":
+    g = Games()
+    g.add_to_readme()
+    g.git_push()
